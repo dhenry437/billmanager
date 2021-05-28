b540699e0655535408a8adb81e86c44737aecfc8
Dan Henry <dhenry437@gmail.com>
2021-05-28 12:45:17 +1000

    simple calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'calendar/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
 
   root 'home#index'
+
+  get 'calendar', to: 'calendar#index'
 end
