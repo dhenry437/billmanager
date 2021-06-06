8189de3ac4c6e7f14edfbd94e09f4b83924bd1bb
Dan Henry <dhenry437@gmail.com>
2021-06-07 09:26:18 +1000

    bill ui use rails helpers

--- app/javascript/packs/bill.js
+++ app/javascript/packs/bill.js
@@ -1,10 +1,7 @@
 $('#recurringCheck').change(function () {
-    console.log('click');
     if (this.checked) {
         $('#recurring').removeClass('visually-hidden');
-        $('#notRecurring').addClass('visually-hidden');
     } else {
         $('#recurring').addClass('visually-hidden');
-        $('#notRecurring').removeClass('visually-hidden');
     }
 })
