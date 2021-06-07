48038752acf5036748282f266dff27468157d201
Dan Henry <dhenry437@gmail.com>
2021-06-08 07:42:55 +1000

    render one time events on calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,4 +2,5 @@ class CalendarController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
+    @events = Bill.where(user_id: current_user.id).all
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,7 @@
 class Bill < ApplicationRecord
   belongs_to :user
+  
+  def start_time
+    date
+  end
 end
