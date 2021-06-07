aeefab97a3aaab2bef3c6855f4bfa7cd81fb3f1d
Dan Henry <dhenry437@gmail.com>
2021-06-07 17:38:21 +1000

    generate recurring yaml

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem "simple_calendar", "~> 2.4"
+gem 'ice_cube'
--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,14 +1,14 @@
 class BillsController < ApplicationController
-  before_action :set_bill, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_bill, only: %i[show edit update destroy]
 
   # GET /bills or /bills.json
   def index
-    @bills = Bill.all
+    @bills = Bill.where(user_id: current_user.id).all
   end
 
   # GET /bills/1 or /bills/1.json
-  def show
-  end
+  def show; end
 
   # GET /bills/new
   def new
@@ -16,13 +16,13 @@ class BillsController < ApplicationController
   end
 
   # GET /bills/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /bills or /bills.json
   def create
-    @bill = Bill.new(bill_params)
+    @bill = Bill.new(name: params[:bill][:name], amount: params[:bill][:amount], date: params[:bill][:date])
     @bill.user_id = current_user.id
+    @bill.recurring = generate_recurring_yaml
 
     respond_to do |format|
       if @bill.save
@@ -38,7 +38,7 @@ class BillsController < ApplicationController
   # PATCH/PUT /bills/1 or /bills/1.json
   def update
     respond_to do |format|
-      if @bill.update(bill_params)
+      if @bill.update(name: params[:bill][:name], amount: params[:bill][:amount], date: params[:bill][:date], recurring: generate_recurring_yaml)
         format.html { redirect_to @bill, notice: "Bill was successfully updated." }
         format.json { render :show, status: :ok, location: @bill }
       else
@@ -58,13 +58,37 @@ class BillsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bill
-      @bill = Bill.find(params[:id])
-    end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bill
+    @bill = Bill.where(user_id: current_user.id).find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def bill_params
-      params.require(:bill).permit(:name, :amount, :date, :recurring, :user_id)
+  def bill_params
+    params.require(:bill).permit(:name, :amount, :date, :recurring, :recurring_bool, :recurring_every, :recurring_period, :occurs_until)
+  end
+
+  def generate_recurring_yaml
+    return unless params[:bill][:recurring_bool] == '1'
+
+    schedule = if params[:bill][:occurs_until].empty? then
+                 IceCube::Schedule.new(Date.parse(params[:bill][:date]))
+               else
+                 IceCube::Schedule.new(Date.parse(params[:bill][:date]), end_time: Date.parse(params[:bill][:occurs_until]))
+               end
+
+    case params[:bill][:recurring_period]
+    when 'daily'
+      schedule.add_recurrence_rule IceCube::Rule.daily(params[:bill][:recurring_every])
+    when 'weekly'
+      schedule.add_recurrence_rule IceCube::Rule.weekly(params[:bill][:recurring_every])
+    when 'monthly'
+      schedule.add_recurrence_rule IceCube::Rule.monthly(params[:bill][:recurring_every])
+    when 'yearly'
+      schedule.add_recurrence_rule IceCube::Rule.yearly(params[:bill][:recurring_every])
     end
+
+    schedule.to_yaml
+  end
 end
