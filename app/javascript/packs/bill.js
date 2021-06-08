7f70c28041c15bbb263d3d8f3131f98f5a780d76
Dan Henry <dhenry437@gmail.com>
2021-06-08 18:03:09 +1000

    validation for bill form

--- app/javascript/packs/bill.js
+++ app/javascript/packs/bill.js
@@ -1,7 +1,9 @@
 $('#recurringCheck').change(function () {
     if (this.checked) {
         $('#recurring').removeClass('visually-hidden');
+        $('#bill_recurring_every').prop('required', true);
     } else {
         $('#recurring').addClass('visually-hidden');
+        $('#bill_recurring_every').prop('required', false);
     }
 })
