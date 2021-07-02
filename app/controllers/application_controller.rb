b82ed3d450ac6cd9ebbd455f2b38eb252b51c8bc
Dan Henry <dhenry437@gmail.com>
2021-07-02 17:21:06 +1000

    redirect sign in sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    '/'
+  end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
 end
