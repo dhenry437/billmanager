fb41c35d98ba782d7fb21afdc1a8fe8e4a1e1750
Dan Henry <dhenry437@gmail.com>
2021-07-16 10:24:21 +1000

    help page

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,16 +1,6 @@
 class CalendarController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @bills = Bill.where(user_id: current_user.id).all
-    @calendar_bills = @bills.flat_map { |x| x.calendar_bills(params.fetch(:start_date, Time.zone.now)) }
-
-    @paydays = Payday.where(user_id: current_user.id).all
-    @calendar_paydays = @paydays.flat_map { |x| x.calendar_paydays(params.fetch(:start_date, Time.zone.now)) }
-
-    @events = @calendar_bills + @calendar_paydays
-  end
-
   def settings
     if params[:show_balance_each_day]
       cookies.permanent[:show_balance_each_day] = params[:show_balance_each_day]
